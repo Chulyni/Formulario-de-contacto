ef99a044c38e3365743951a9e9724e694b8bf261
Luis <chuli18cifuentesgmail.com>
2024-06-03 21:09:51 -0400

    Nueva actualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleCheck } from "@fortawesome/free-regular-svg-icons";
-import "./App.css";
 
 function App() {
   const [errors, setErrors] = useState({});
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        buttonGreen: "hsl(169, 82%, 27%)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
